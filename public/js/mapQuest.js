6352eedb83950595ec1ce4c7ace9e97ec4121d9b
Viviana Davila <vdavila@ucsd.edu>
2019-02-28 10:31:52 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'pES67bynAZfQsJqZM43VinQfKHub68oW';
+
+	var map = L.mapquest.map('map', {
+		center: [32.878924, -117.235911],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.878924, -117.235911]).addTo(map);
 }
